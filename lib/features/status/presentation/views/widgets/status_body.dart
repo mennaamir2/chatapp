87a47348a82f40b861427994805bc7f1733d85c8
Menna Amir <mennaamir72@gmail.com>
2024-11-03 03:56:52 +0200

    edit status

--- lib/features/status/presentation/views/widgets/status_body.dart
+++ lib/features/status/presentation/views/widgets/status_body.dart
@@ -1,7 +1,7 @@
+import 'package:chatapp1/core/shared_widgets/grey_title_with_padding.dart';
 import 'package:chatapp1/features/status/presentation/views/widgets/add_status_list_tile.dart';
 import 'package:chatapp1/features/status/presentation/views/widgets/muted_updates_list_view.dart';
 import 'package:chatapp1/features/status/presentation/views/widgets/recent_updates_list_view.dart';
-import 'package:chatapp1/features/status/presentation/views/widgets/updates_title.dart';
 import 'package:chatapp1/features/status/presentation/views/widgets/viewed_updates_list_view.dart';
 import 'package:flutter/material.dart';
 class StatusBody extends StatelessWidget {
@@ -10,14 +10,14 @@ class StatusBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  ListView(
-      children: const [
-        AddStatusListTile(),
-        UpdatesTitle(title: 'Recent updates'),
-        RecentUpdatesListView(),
-        UpdatesTitle(title: 'Viewed updates'),
-        ViewedUpdatesListView(),
-        UpdatesTitle(title: 'Muted updates'),
-        MutedUpdatesListView()
+      children:  [
+        const AddStatusListTile(),
+        greyTitleWithPadding(title: 'Recent updates'),
+        const RecentUpdatesListView(),
+        greyTitleWithPadding(title: 'Viewed updates'),
+        const ViewedUpdatesListView(),
+        greyTitleWithPadding(title: 'Muted updates'),
+        const MutedUpdatesListView()
       ],
     );
   }
